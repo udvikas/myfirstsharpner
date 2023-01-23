4c9925364ac0c2c194cebc7288a1dd60bbcb823e
Vikash Sharma <kvikashsharma09@gmail.com>
2023-01-23 19:20:00 +0530

    DOM folder is added

---
+++ DOM/main.js
@@ -0,0 +1,21 @@
+
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelectorAll('.item'))
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => {console.log(item)});
+
+// const ul = document.querySelector('.items');
+// // ul.remove();.
+// // ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'vikas';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+// const btn = document.querySelector('.btn');
+// btn.style.backgroundColor = 'green';
+
+const ul = document.querySelector('.items');
+ul.firstElementChild.textContent = 'Hello';
+ul.firstElementChild.style.color = 'green';
+ul.children[1].style.backgroundColor = 'yellow';
